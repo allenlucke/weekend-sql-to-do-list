78aac25b2ed7ebae8c104ad932755000b834b04a
Allen Lucke <allenlucke@gmail.com>
2019-11-29 14:53:32 -0600

    router.post functioning properly

--- server/routers/to_do.router.js
+++ server/routers/to_do.router.js
@@ -6,7 +6,7 @@ const pool = require('../modules/pool');
 //API Calls
 // GET
 router.get('/', (req, res) => {
-    pool.query(`SELECT * FROM "to_do_list" ORDER BY "completed" ASC;`)
+    pool.query(`SELECT * FROM "to_do_list" ORDER BY "completed" DESC;`)
     .then((response) => {
         res.send(response.rows);
     })
@@ -16,7 +16,22 @@ router.get('/', (req, res) => {
 });
 
 //POST
-router.post('/' (req, res))
+router.post('/', (req, res) => {
+    const newTask = req.body;
+    console.log(newTask);
+    const queryString = `INSERT INTO "to_do_list" ("task", "completed")
+                        VALUES ('${newTask.task}', '${newTask.completed}');`;
+
+    pool.query(queryString)  
+    .then((response) => {
+        res.sendStatus(201);
+    })
+    .catch((err) => {
+        res.sendStatus(500);
+    })
+});
+
+    
 
 
 
